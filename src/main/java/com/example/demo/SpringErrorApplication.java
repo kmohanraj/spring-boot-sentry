f87ca7f9295ef67d5953f235834d25811970e2ff
mohanraj kanthasamy <mohanraj.kandhasamy@crystaldelta.com>
2021-04-22 08:27:24 +0530

    sentry integration to spring boot app

---
+++ src/main/java/com/example/demo/SpringErrorApplication.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import io.sentry.spring.SentryServletContextInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+
+@Controller
+@SpringBootApplication
+public class SpringErrorApplication {
+	private static final Logger logger = LoggerFactory.getLogger(SpringErrorApplication.class);
+	@Bean
+	public HandlerExceptionResolver sentryExceptionResolver() {
+		return new io.sentry.spring.SentryExceptionResolver();
+	}
+
+	@Bean
+	public SentryServletContextInitializer sentryServletContextInitializer() {
+		return new io.sentry.spring.SentryServletContextInitializer();
+	}
+
+	@GetMapping("/")
+	String home(@RequestParam(required = false) String name) {
+		logger.info("SENTRY ERROR INFO");
+		return "Hello " + name.toUpperCase();
+	}
+
+//	@GetMapping
+//	public void index() {
+//		throw new NullPointerException();
+//	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringErrorApplication.class, args);
+	}
+
+}
